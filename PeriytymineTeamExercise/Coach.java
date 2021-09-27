8bb4acc8520f361eabf82767ee2c89484661f299
repsukka <81526112+repsukka@users.noreply.github.com>
2021-09-27 13:21:53 +0300

    team

---
+++ PeriytymineTeamExercise/Coach.java
@@ -0,0 +1,12 @@
+public class Coach extends TeamMember {
+    
+    private int experience;
+
+    public Coach(String name, int experience) {
+        super(name);
+    }
+    this.experience = experience;
+
+    
+
+}
